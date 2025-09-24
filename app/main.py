e1cfe50d1ffbe4051c490b03266236db2294f6e5
Kevin Smith <vndinmchine@gmail.com>
2025-09-24 07:23:54 -0400

    Create main.py

---
+++ app/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from starlette.middleware.sessions import SessionMiddleware
+from fastapi.staticfiles import StaticFiles
+from app.middleware import auth_middleware
+from app.auth import router as auth_router
+from app.utils.security import SESSION_SECRET
+
+app = FastAPI()
+
+app.add_middleware(SessionMiddleware, secret_key=SESSION_SECRET)
+app.middleware("http")(auth_middleware)
+
+app.include_router(auth_router)
+
+# Static files (favicon, css, etc.)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+async def home():
+    return {"message": "Welcome to InsightHub. You're logged in."}
