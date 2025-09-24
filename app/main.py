5a81d09cf4a04fd7f40fb23b3a77969a149b63a8
Kevin Smith <vndinmchine@gmail.com>
2025-09-24 13:08:16 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -70,7 +70,7 @@ async def parse_with_header(
 
         # Manually set header row
         header_values = df_raw.iloc[header_row - 1].tolist()
-        print("DEBUG — header_row_values:", header_row_values)
+        print("DEBUG — header_row_values:", header_values)
 
         # Remove all rows before header
         df = df_raw.iloc[header_row:].copy()
